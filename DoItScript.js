da5c7ba8cf626dd188f819aff129a3bf31069d62
ToudouSeiya <stwinavenger@gmail.com>
2025-12-11 15:11:20 -0500

    Create DoItScript.js

---
+++ DoItScript.js
@@ -0,0 +1,8 @@
+//I gave up on animation :/
+function react() {
+	document.getElementById("solid").style.opacity = "0";
+	document.getElementById("half").style.opacity = "0";
+	document.getElementById("third").style.opacity = "0";
+	document.getElementById("left").style.opacity = "1";
+	document.getElementById("right").style.opacity = "1";
+}
